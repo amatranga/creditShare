00600fb11da2a9a17688b05c52a60da23108f083
amatranga <amatranga892@gmail.com>
2017-10-09 17:34:08 -0700

    Implement basic .tsx component skeleton

---
+++ src/components/CreditShare.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import MyCredentials from './MyCredentials';
+import SharedWithMe from './SharedWithMe';
+import SharedWithOthers from './SharedWithOthers';
+import Navbar from './Navbar';
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CreditShare from './components/CreditShare';
+
+ReactDOM.render(<CreditShare />, document.getElementById('app'));
