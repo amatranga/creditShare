442b9465eb085dfc2762156548eb7a1a706701ec
amatranga <amatranga892@gmail.com>
2017-10-11 16:38:27 -0700

    Implements SharedWithMe component

---
+++ src/components/SharedWithMe.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Credentials from './Credentials';
+
+export default class SharedWithMe extends React.Component<any, any> {
+  constructor(props:any) {
+    super(props);
+    this.state = {
+      SharedWithMe: [this.props.SharedWithMe]
+    }
+  }
+  render() {
+    return (
+      <span className="col">
+        <h3>Shared With Me</h3>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>Website</th>
+              <th>UserName</th>
+              <th>Password</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.sharedWithMe[0].map((credential:any, idx:any) =>
+              <Credentials key={idx} id={idx} credential={credential} />
+            )}  
+          </tbody>
+        </table>
+      </span>
+    );
+  }
+}
