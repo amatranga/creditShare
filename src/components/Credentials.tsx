8cf2e5146085b37e6e46c7a0fba820e6d466f760
amatranga <amatranga892@gmail.com>
2017-10-11 16:34:11 -0700

    Implements Credentials comoponent

---
+++ src/components/Credentials.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+
+const Credentials = (props:any) => {
+  if (props.onDelete) {
+    return (
+      <tr>
+        <td>
+          {props.credential.website}
+        </td>
+        <td>
+          {props.credential.username}
+        </td>
+        <td>
+          {props.credential.password_id}
+        </td>
+        <td>
+          <button id={props.id} className="btn btn-primary" onClick={props.onShare ? props.onShare : null}>Share</button>
+        </td>
+        <td>
+          <button id={props.id} className="btn btn-danger" onClick={props.onDelete}>Delete</button>
+        </td>
+      </tr>
+    );
+  } else {
+    return (
+      <tr>
+        <td>
+          {props.credential.website}
+        </td>
+        <td>
+          {props.credential.username}
+        </td>
+        <td>
+          {props.credential.password_id}
+        </td>
+      </tr>
+    );
+  }
+}
+
+export default Credentials;
